6d4f8a5ef9e4f7abb3ad48a8e70b638c0dc5e554
rlabausa <rubylabausa@gmail.com>
2019-09-01 23:18:00 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # slackmoji-scraper
-Web scraper for slackmojis.com
+This is a simple program that automates the process of downloading all emojis from https://slackmojis.com/. 
+
+To run the program, install the required packages. This can be done by running the following command from the root of the project directory:
+```Bash
+pip3 install -r requirements.txt
+``` 
+Then execute the script with your Python interpreter:
+```Bash
+python3 scrape.py
+```
+You can also choose to make the file a standalone executable by running:
+```Bash
+# make scrape.py executable
+chmod +x scrape.py
+
+# run the program (note: this requires setting the absolute path to interpreter in the script)
+./scrape.py
+```
+
+
---
+++ scrape.py
@@ -0,0 +1,37 @@
+#!/path/to/python3/virtual/env
+
+import os
+import getpass
+
+import requests
+from lxml import html
+
+# set Downloads folder as img save destination
+dest_folder = f'/home/{getpass.getuser()}/Downloads/slackmojis/'
+
+try:
+    os.makedirs(dest_folder)
+    print('Directory Created')
+
+except FileExistsError:
+    print('Directory Already Exists')
+
+# download main html page and conver into tree
+html_page = requests.get('https://slackmojis.com/categories/19-random-emojis')
+html_tree = html.fromstring(html_page.text)
+
+# select all elements with xpath
+elements = html_tree.xpath('//a[@class="downloader"]')
+
+# download each img 
+for el in elements:
+    url = requests.get(url=el.attrib['href'])
+    filename = el.attrib['download']
+    filepath = dest_folder + filename
+    
+    with open(filepath, 'wb') as fout:
+        fout.write(url.content)
+    
+    print(filepath)
+
+print('\nDONE')
